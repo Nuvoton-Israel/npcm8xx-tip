8eb6f7257e5616a764438cc58acb507416273437
xilingsun <xilingsun@microsoft.com>
2026-08-11 20:35:46 -0700

    composite-eat: verify ES384 interoperability

--- tip/common/composite_eat/testing/composite_eat_generator_test.c
+++ tip/common/composite_eat/testing/composite_eat_generator_test.c
@@ -5,14 +5,42 @@
 #include <stdint.h>
 #include <string.h>
 
+#include <openssl/evp.h>
+
 #include "testing.h"
 #include "asn1/ecc_der_util.h"
+#include "crypto/ecc_openssl.h"
+#include "crypto/hash_openssl.h"
 #include "composite_eat/composite_eat_generator.h"
 #include "qcbor/qcbor_decode.h"
 #include "qcbor/qcbor_spiffy_decode.h"
 
 
 TEST_SUITE_LABEL ("composite_eat_generator");
+
+static const uint8_t test_ecc384_public_key[] = {
+    0x30, 0x76, 0x30, 0x10, 0x06, 0x07, 0x2a, 0x86, 0x48, 0xce, 0x3d, 0x02, 0x01, 0x06, 0x05,
+    0x2b, 0x81, 0x04, 0x00, 0x22, 0x03, 0x62, 0x00, 0x04, 0xcf, 0x6b, 0x8d, 0x9a, 0x48, 0x75,
+    0xa9, 0x5a, 0x19, 0x89, 0x72, 0x18, 0xa4, 0x94, 0x4d, 0xef, 0x0a, 0x93, 0xce, 0x5b, 0x8b,
+    0x8d, 0xf1, 0x37, 0x54, 0x09, 0x17, 0x89, 0xbc, 0xef, 0x69, 0xdb, 0x6c, 0xa7, 0x9e, 0xf6,
+    0xb6, 0x4b, 0x5c, 0x13, 0xed, 0x3c, 0xbf, 0xed, 0x0b, 0x3d, 0xf1, 0x7e, 0x53, 0xbf, 0xf4,
+    0x76, 0x31, 0x31, 0x33, 0xa3, 0x58, 0x3c, 0x11, 0x3d, 0xeb, 0x8d, 0xb6, 0xb7, 0x47, 0x4a,
+    0xe3, 0x51, 0xd0, 0x38, 0x26, 0xac, 0xec, 0x11, 0x34, 0x33, 0x04, 0x0d, 0xc6, 0xc3, 0x75,
+    0x37, 0xa1, 0x89, 0xdd, 0x4f, 0x66, 0x57, 0x72, 0xac, 0xc5, 0x3b, 0xb6, 0xc6, 0xb8, 0x0c,
+};
+
+static const uint8_t test_ecc384_private_key[] = {
+    0x30, 0x81, 0xa4, 0x02, 0x01, 0x01, 0x04, 0x30, 0x5d, 0x57, 0x6f, 0x7d, 0xd2, 0x90, 0xd7, 0x94, 0x53,
+    0x02, 0x74, 0x38, 0xb3, 0xe6, 0x81, 0xa1, 0x10, 0xc5, 0xd0, 0x95, 0x31, 0x3d, 0xcb, 0x69, 0x78, 0x34,
+    0x41, 0x62, 0x2f, 0x87, 0xc7, 0x1e, 0x12, 0x80, 0x4c, 0xb0, 0x97, 0x2e, 0x63, 0xbd, 0xff, 0x8a, 0x25,
+    0x6b, 0x63, 0xd4, 0x90, 0x8d, 0xa0, 0x07, 0x06, 0x05, 0x2b, 0x81, 0x04, 0x00, 0x22, 0xa1, 0x64, 0x03,
+    0x62, 0x00, 0x04, 0xcf, 0x6b, 0x8d, 0x9a, 0x48, 0x75, 0xa9, 0x5a, 0x19, 0x89, 0x72, 0x18, 0xa4, 0x94,
+    0x4d, 0xef, 0x0a, 0x93, 0xce, 0x5b, 0x8b, 0x8d, 0xf1, 0x37, 0x54, 0x09, 0x17, 0x89, 0xbc, 0xef, 0x69,
+    0xdb, 0x6c, 0xa7, 0x9e, 0xf6, 0xb6, 0x4b, 0x5c, 0x13, 0xed, 0x3c, 0xbf, 0xed, 0x0b, 0x3d, 0xf1, 0x7e,
+    0x53, 0xbf, 0xf4, 0x76, 0x31, 0x31, 0x33, 0xa3, 0x58, 0x3c, 0x11, 0x3d, 0xeb, 0x8d, 0xb6, 0xb7, 0x47,
+    0x4a, 0xe3, 0x51, 0xd0, 0x38, 0x26, 0xac, 0xec, 0x11, 0x34, 0x33, 0x04, 0x0d, 0xc6, 0xc3, 0x75, 0x37,
+    0xa1, 0x89, 0xdd, 0x4f, 0x66, 0x57, 0x72, 0xac, 0xc5, 0x3b, 0xb6, 0xc6, 0xb8, 0x0c,
+};
 
 static uint8_t alias_key[] = {0xa0, 0xa1, 0xa2, 0xa3};
 static uint8_t alias_certificate[64];
@@ -440,6 +468,92 @@ static void composite_eat_generator_test_pcr_evidence_algorithms (CuTest *test)
 	CuAssertIntEquals (test, COMPOSITE_EAT_SHA384_LENGTH, digest.len);
 }
 
+static void composite_eat_generator_test_es384_interoperability (CuTest *test)
+{
+	static struct composite_eat_generator_workspace workspace;
+	struct composite_eat_generator generator;
+	struct ecc_engine_openssl ecc;
+	struct hash_engine_openssl hash;
+	struct hash_engine_openssl_state hash_state;
+	struct ecc_public_key public_key;
+	uint8_t request[256];
+	uint8_t response[4096];
+	uint8_t sig_structure[COMPOSITE_EAT_PAYLOAD_MAX + COMPOSITE_EAT_PROTECTED_MAX + 32];
+	uint8_t digest[COMPOSITE_EAT_SHA384_LENGTH];
+	uint8_t der_signature[ECC_DER_ECDSA_MAX_LENGTH];
+	uint8_t bstr_header[3];
+	size_t request_length;
+	size_t response_length;
+	size_t sig_structure_length = 0;
+	size_t header_length;
+	unsigned int digest_length = 0;
+	int der_length;
+	int verify_status;
+	QCBORDecodeContext decoder;
+	UsefulBufC protected_headers;
+	UsefulBufC payload;
+	UsefulBufC signature;
+	enum composite_eat_generator_status status;
+
+	TEST_START;
+	composite_eat_generator_test_reset ();
+	test_keys.alias_key = test_ecc384_private_key;
+	test_keys.alias_key_length = sizeof (test_ecc384_private_key);
+	request_length = composite_eat_generator_test_request (request, sizeof (request));
+	CuAssertIntEquals (test, 0, ecc_openssl_init (&ecc));
+	CuAssertIntEquals (test, 0, hash_openssl_init (&hash, &hash_state));
+	status = composite_eat_generator_init_with_attestation (&generator, &ecc.base, &hash.base,
+		&test_riot, &test_pcr_store, &workspace, &test_attestation);
+	CuAssertIntEquals (test, COMPOSITE_EAT_GENERATOR_OK, status);
+	status = composite_eat_generate (&generator, request, request_length, response,
+		sizeof (response), &response_length);
+	CuAssertIntEquals (test, COMPOSITE_EAT_GENERATOR_OK, status);
+
+	QCBORDecode_Init (&decoder, (UsefulBufC){response + 1, response_length - 1},
+		QCBOR_DECODE_MODE_NORMAL);
+	QCBORDecode_EnterArray (&decoder, NULL);
+	QCBORDecode_GetByteString (&decoder, &protected_headers);
+	QCBORDecode_EnterMap (&decoder, NULL);
+	QCBORDecode_ExitMap (&decoder);
+	QCBORDecode_GetByteString (&decoder, &payload);
+	QCBORDecode_GetByteString (&decoder, &signature);
+	QCBORDecode_ExitArray (&decoder);
+	CuAssertIntEquals (test, QCBOR_SUCCESS, QCBORDecode_Finish (&decoder));
+
+	memcpy (&sig_structure[sig_structure_length],
+		(const uint8_t[]){0x84, 0x6a, 'S', 'i', 'g', 'n', 'a', 't', 'u', 'r', 'e', '1'}, 12);
+	sig_structure_length += 12;
+	header_length = composite_eat_generator_test_bstr_header (protected_headers.len, bstr_header);
+	memcpy (&sig_structure[sig_structure_length], bstr_header, header_length);
+	sig_structure_length += header_length;
+	memcpy (&sig_structure[sig_structure_length], protected_headers.ptr, protected_headers.len);
+	sig_structure_length += protected_headers.len;
+	sig_structure[sig_structure_length++] = 0x40;
+	header_length = composite_eat_generator_test_bstr_header (payload.len, bstr_header);
+	memcpy (&sig_structure[sig_structure_length], bstr_header, header_length);
+	sig_structure_length += header_length;
+	memcpy (&sig_structure[sig_structure_length], payload.ptr, payload.len);
+	sig_structure_length += payload.len;
+	CuAssertIntEquals (test, 1,
+		EVP_Digest (sig_structure, sig_structure_length, digest, &digest_length, EVP_sha384 (),
+			NULL));
+	CuAssertIntEquals (test, COMPOSITE_EAT_SHA384_LENGTH, digest_length);
+
+	der_length = ecc_der_encode_ecdsa_signature (signature.ptr,
+		&((const uint8_t *) signature.ptr)[COMPOSITE_EAT_SHA384_LENGTH],
+		COMPOSITE_EAT_SHA384_LENGTH, der_signature, sizeof (der_signature));
+	CuAssertTrue (test, der_length > 0);
+	CuAssertIntEquals (test, 0,
+		ecc.base.init_public_key (&ecc.base, test_ecc384_public_key,
+			sizeof (test_ecc384_public_key), &public_key));
+	verify_status = ecc.base.verify (&ecc.base, &public_key, digest, sizeof (digest), der_signature,
+		(size_t) der_length);
+	CuAssertIntEquals (test, 0, verify_status);
+	ecc.base.release_key_pair (&ecc.base, NULL, &public_key);
+	hash_openssl_release (&hash);
+	ecc_openssl_release (&ecc);
+}
+
 static void composite_eat_generator_test_failures (CuTest *test)
 {
 	static struct composite_eat_generator_workspace workspace;
@@ -553,6 +667,7 @@ TEST_SUITE_START (composite_eat_generator);
 TEST (composite_eat_generator_test_happy_path);
 TEST (composite_eat_generator_test_buffer_too_small);
 TEST (composite_eat_generator_test_pcr_evidence_algorithms);
+TEST (composite_eat_generator_test_es384_interoperability);
 TEST (composite_eat_generator_test_failures);
 TEST (composite_eat_generator_test_hash_cleanup);
 TEST (composite_eat_generator_test_init_and_busy);
